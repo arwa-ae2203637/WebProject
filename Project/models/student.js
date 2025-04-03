1c87948f13eba58d5eeb21fa88257f7199e037d5
sa2203149 <sa2203149@qu.edu.qa>
2025-04-03 12:25:10 +0300

    fixed classes

--- Project/models/student.js
+++ Project/models/student.js
@@ -1,38 +1,58 @@
 import { User } from "models/user.js";
 
-class Student extends User{
-  completedCourses = [];
-  currentCourses = [];
-  pendingCourses = [];
+export class Student extends User{
+  #completedCourses = [];
+  #currentCourses = [];
+  #pendingCourses = [];
 
-  constructor(name, email, password, userType, completedCourses, currentCourses, pendingCourses){
-    super(name, email, password, userType);
-    this.completedCourses = completedCourses;
-    this.currentCourses = currentCourses;
-    this.pendingCourses = pendingCourses;
+  constructor(obj){
+    super({...obj, userType: "student"});
+    this.#completedCourses = obj?.completedCourses ?? [];
+    this.#currentCourses = obj?.currentCourses ?? [];
+    this.#pendingCourses = obj?.pendingCourses ?? [];
   }
 
+  // SETTERS AND GETTERS
+  get completedCourses(){
+    return this.#completedCourses;
+  }
+  get currentCourses(){
+    return this.#currentCourses;
+  }
+  get pendingCourses(){
+    return this.#pendingCourses;
+  }
+  set completedCourses(completedCourses){
+    this.#completedCourses = completedCourses;
+  }
+  set currentCourses(currentCourses){
+    this.#currentCourses = currentCourses;
+  }
+  set pendingCourses(pendingCourses){
+    this.#pendingCourses = pendingCourses;
+  }
+
+  // FROM JSON
   static fromJSON(json){
     return JSON.parse(json);
   }
 
+  // TO JSON
   toJson(){
     return {
       ...super.toJson(),
-      completedCourses: this.completedCourses,
-      currentCourses: this.currentCourses,
-      pendingCourses: this.pendingCourses
+      completedCourses: this.#completedCourses,
+      currentCourses: this.#currentCourses,
+      pendingCourses: this.#pendingCourses,
     };
   }
 
-  static toString() {
-    return `Student [ID: ${this.id}, 
-            Name: ${this.name}, 
-            Username: ${this.username}, 
-            Type: ${this.userType},
-            Completed Courses: ${this.completedCourses},
-            Current Courses: ${this.currentCourses},
-            Pending Courses: ${this.pendingCourses}]`;
+  // TO STRING
+  toString() {
+    return `${super.toString()}, 
+            Completed Courses: ${this.#completedCourses.length}, 
+            Current Courses: ${this.#currentCourses.length}, 
+            Pending Courses: ${this.#pendingCourses.length},`;
   }
-
+  
 }
--- Project/models/user.js
+++ Project/models/user.js
@@ -1,46 +1,76 @@
 import { customAlphabet } from 'nanoid';
 
 export class User{
-  id;
-  name;
-  email;
-  password;
-  userType;
+  #id;
+  #name;
+  #email;
+  #password;
+  #userType;
 
-  constructor(name, email, password, userType){
-    this.id = generateUserId();
-    this.name = name;
-    this.email = email;
-    this.password = password;
-    this.userType = userType;
+  constructor(obj){
+    if(this.constructor === User){
+      throw new Error("Can't instantiate abstract class");
+    }
+    this.#id = User.generateId(this.#userType);
+    this.#name = obj?.name ?? "";
+    this.#email = obj?.email ?? "";
+    this.#password = obj?.password ?? "";
+    this.#userType = obj?.userType ?? "";
   }
 
-  static generateId() {
-    const numId = customAlphabet('0123456789', 9);
-    if(this.userType === 'admin') return `ADMIN-${numId()}`;
-    else if(this.userType === 'student') return `STUD-${numId()}`;
+  // SETTERS AND GETTERS
+  get id(){
+    return this.#id;
+  }
+  get name(){
+    return this.#name;
+  }
+  get email(){
+    return this.#email;
+  }
+  get password(){
+    return this.#password;
+  }
+  get userType(){
+    return this.#userType;
+  }
+  set name(name){
+    this.#name = name;
+  }
+  set email(email){
+    this.#email = email;
+  }
+  set password(password){
+    this.#password = password;
+  }
+  set userType(userType){
+    this.#userType = userType;
+  }
+
+  // GENERATE USER ID
+  static generateId(userType) {
+    const numId = customAlphabet('0123456789', 8);
+    if (userType === 'admin') return `ADMIN-${numId()}`;
+    else if (userType === 'student') return `STUD-${numId()}`;
     else return `INST-${numId()}`;
-  };
-
-  static fromJSON(json){
-    return JSON.parse(json);
   }
+
 
   toJson(){
     return {
-      id: this.id,
-      name: this.name,
-      email: this.email,
-      password: this.password,
-      userType: this.userType
+      id: this.#id,
+      name: this.#name,
+      email: this.#email,
+      password: this.#password,
+      userType: this.#userType
     };
   }
 
-  static toString() {
-    return `User [ID: ${this.id}, 
-            Name: ${this.name}, 
-            Username: ${this.username}, 
-            Type: ${this.userType}]`;
+  toString() {
+    return `UserType: ${this.#userType}
+            ID: ${this.#id}, 
+            Name: ${this.#name}, 
+            Email: ${this.#email},`;
   }
   
 }
