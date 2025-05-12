5fe6568559cc0012e214d07f20e9b44c0b6e1070
aelaradi__ <ae2203637@qu.edu.qa>
2025-05-12 21:40:34 +0300

    statistics user interface

--- web-project/app/api/stats/highestAverageGrade/route.js
+++ web-project/app/api/stats/highestAverageGrade/route.js
@@ -1,9 +1,9 @@
-import { getMostImprovedStudent } from '@/repo/statistics.js';
+import { getStudentWithHighestAverageGrade } from '@/repo/statistics.js';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const data = await getMostImprovedStudent();
+    const data = await getStudentWithHighestAverageGrade();
     return NextResponse.json(data, { status: 200 });
   } catch (e) {
     return NextResponse.json({ error: e.message }, { status: 500 });
